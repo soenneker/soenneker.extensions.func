6d8362a766547fe9ac50821d0b70f385330512cf
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:20 +0000

    Tests

---
+++ test/Soenneker.Extensions.Func.Tests/FuncExtensionsTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Func.Tests;
+
+public class FuncExtensionsTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
